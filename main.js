2d859b7fd650365c662fe5853d6cf0518df76886
Legedy1 <legendaryelix3r@gmail.com>
2022-08-29 18:35:18 +1000

    fart fr

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-var c = document.getElementById("display");
-var ctx = c.getContext("2d");
+var PI = Math.PI;
+var TAU = PI * 2;
+
+var SHADOW_LENGTH = 2000;
+
+var Canvas = document.getElementById("display");
+var Draw = Canvas.getContext("2d");
 
 function resize() {
-	var box = c.getBoundingClientRect();
-	c.width = box.width;
-	c.height = box.height;
+	var box = Canvas.getBoundingClientRect();
+	Canvas.width = box.width;
+	Canvas.height = box.height;
 }
 
-var light = {
+var LightPosition = {
 	x: 160,
 	y: 200
 }
@@ -15,93 +20,81 @@ var light = {
 var colors = ["#f5c156", "#e6616b", "#5cd3ad"];
 
 function drawLight() {
-	ctx.beginPath();
-	ctx.arc(light.x, light.y, 2000, 0, 2 * Math.PI);
-	var gradient = ctx.createRadialGradient(light.x, light.y, 0, light.x, light.y, 1000);
+	Draw.beginPath();
+	Draw.arc(LightPosition.x, LightPosition.y, 2000, 0, 2 * Math.PI);
+	var gradient = Draw.createRadialGradient(LightPosition.x, LightPosition.y, 0, LightPosition.x, LightPosition.y, 1000);
 	gradient.addColorStop(0, "#3b4654");
 	gradient.addColorStop(1, "#2c343f");
-	ctx.fillStyle = gradient;
-	ctx.fill();
-
-	ctx.beginPath();
-	ctx.arc(light.x, light.y, 20, 0, 2 * Math.PI);
-	gradient = ctx.createRadialGradient(light.x, light.y, 0, light.x, light.y, 5);
-	gradient.addColorStop(0, "#fff");
-	gradient.addColorStop(1, "#3b4654");
-	ctx.fillStyle = gradient;
-	ctx.fill();
+	Draw.fillStyle = gradient;
+	Draw.fill();
 }
 
 function Box() {
-	this.half_size = Math.floor((Math.random() * 50) + 1);
-	this.x = Math.floor((Math.random() * c.width) + 1);
-	this.y = Math.floor((Math.random() * c.height) + 1);
-	this.r = Math.random() * Math.PI;
-	this.shadow_length = 2000;
+	this.HalfSize = Math.floor((Math.random() * 50) + 1);
+	this.Position = {
+		X: Math.floor((Math.random() * Canvas.width) + 1),
+		Y: Math.floor((Math.random() * Canvas.height) + 1)
+	};
+	this.Rotation = Math.random() * Math.PI;
 	this.color = colors[Math.floor((Math.random() * colors.length))];
-  
-	this.getDots = function() {
 
-		var full = (Math.PI * 2) / 4;
-
-
-		var p1 = {
-			x: this.x + this.half_size * Math.sin(this.r),
-			y: this.y + this.half_size * Math.cos(this.r)
-		};
-		var p2 = {
-			x: this.x + this.half_size * Math.sin(this.r + full),
-			y: this.y + this.half_size * Math.cos(this.r + full)
-		};
-		var p3 = {
-			x: this.x + this.half_size * Math.sin(this.r + full * 2),
-			y: this.y + this.half_size * Math.cos(this.r + full * 2)
-		};
-		var p4 = {
-			x: this.x + this.half_size * Math.sin(this.r + full * 3),
-			y: this.y + this.half_size * Math.cos(this.r + full * 3)
-		};
+	this.getDots = function () {
+		var PosOffsetX = this.Position.X + this.HalfSize;
+		var PosOffsetY = this.Position.Y + this.HalfSize;
+		var quarter = TAU / 4;
 
 		return {
-			p1: p1,
-			p2: p2,
-			p3: p3,
-			p4: p4
+			p1: {
+				X: PosOffsetX * Math.sin(this.Rotation),
+				Y: PosOffsetY * Math.cos(this.Rotation)
+			},
+			p2: {
+				X: PosOffsetX * Math.sin(this.Rotation + quarter),
+				Y: PosOffsetY * Math.cos(this.Rotation + quarter)
+			},
+			p3: {
+				X: PosOffsetX * Math.sin(this.Rotation + quarter * 2),
+				Y: PosOffsetY * Math.cos(this.Rotation + quarter * 2)
+			},
+			p4: {
+				X: PosOffsetX * Math.sin(this.Rotation + quarter * 3),
+				Y: PosOffsetY * Math.cos(this.Rotation + quarter * 3)
+			}
 		};
 	}
-	this.rotate = function() {
-		var speed = (60 - this.half_size) / 20;
-		this.r += speed * 0.002;
-		this.x += speed;
-		this.y += speed;
+	this.Step = function () {
+		var speed = (60 - this.HalfSize) / 20;
+		this.Rotation += speed * 0.002;
+		this.Position.X += speed;
+		this.Position.Y += speed;
 	}
-	this.draw = function() {
+	this.draw = function () {
 		var dots = this.getDots();
-		ctx.beginPath();
-		ctx.moveTo(dots.p1.x, dots.p1.y);
-		ctx.lineTo(dots.p2.x, dots.p2.y);
-		ctx.lineTo(dots.p3.x, dots.p3.y);
-		ctx.lineTo(dots.p4.x, dots.p4.y);
-		ctx.fillStyle = this.color;
-		ctx.fill();
+		Draw.beginPath();
+		Draw.moveTo(dots.p1.X, dots.p1.Y);
+		Draw.lineTo(dots.p2.X, dots.p2.Y);
+		Draw.lineTo(dots.p3.X, dots.p3.Y);
+		Draw.lineTo(dots.p4.X, dots.p4.Y);
+		Draw.fillStyle = this.color;
+		Draw.fill();
 
 
-		if (this.y - this.half_size > c.height) {
-			this.y -= c.height + 100;
+		if (this.Position.Y - this.HalfSize > Canvas.height) {
+			this.Position.Y -= Canvas.height + 100;
 		}
-		if (this.x - this.half_size > c.width) {
-			this.x -= c.width + 100;
+		if (this.Position.X - this.HalfSize > Canvas.width) {
+			this.Position.X -= Canvas.width + 100;
 		}
 	}
-	this.drawShadow = function() {
+	this.drawShadow = function () {
 		var dots = this.getDots();
 		var angles = [];
 		var points = [];
 
 		for (dot in dots) {
-			var angle = Math.atan2(light.y - dots[dot].y, light.x - dots[dot].x);
-			var endX = dots[dot].x + this.shadow_length * Math.sin(-angle - Math.PI / 2);
-			var endY = dots[dot].y + this.shadow_length * Math.cos(-angle - Math.PI / 2);
+			var angle = Math.atan2(LightPosition.y - dots[dot].y, LightPosition.x - dots[dot].x);
+			var endX = dots[dot].x + SHADOW_LENGTH * Math.sin(-angle - Math.PI / 2);
+			var endY = dots[dot].y + SHADOW_LENGTH * Math.cos(-angle - Math.PI / 2);
 			angles.push(angle);
 			points.push({
 				endX: endX,
@@ -113,13 +106,13 @@ function Box() {
 
 		for (var i = points.length - 1; i >= 0; i--) {
 			var n = i == 3 ? 0 : i + 1;
-			ctx.beginPath();
-			ctx.moveTo(points[i].startX, points[i].startY);
-			ctx.lineTo(points[n].startX, points[n].startY);
-			ctx.lineTo(points[n].endX, points[n].endY);
-			ctx.lineTo(points[i].endX, points[i].endY);
-			ctx.fillStyle = "#2c343f";
-			ctx.fill();
+			Draw.beginPath();
+			Draw.moveTo(points[i].startX, points[i].startY);
+			Draw.lineTo(points[n].startX, points[n].startY);
+			Draw.lineTo(points[n].endX, points[n].endY);
+			Draw.lineTo(points[i].endX, points[i].endY);
+			Draw.fillStyle = "#2c343f";
+			Draw.fill();
 		};
 	}
 }
@@ -127,11 +120,11 @@ function Box() {
 var boxes = [];
 
 function draw() {
-	ctx.clearRect(0, 0, c.width, c.height);
+	Draw.clearRect(0, 0, Canvas.width, Canvas.height);
 	drawLight();
 
 	for (var i = 0; i < boxes.length; i++) {
-		boxes[i].rotate();
+		boxes[i].Step();
 		boxes[i].drawShadow();
 	};
 	for (var i = 0; i < boxes.length; i++) {
@@ -149,21 +142,21 @@ while (boxes.length < 14) {
 }
 
 window.onresize = resize;
-c.onmousemove = function(e) {
-	light.x = e.offsetX == undefined ? e.layerX : e.offsetX;
-	light.y = e.offsetY == undefined ? e.layerY : e.offsetY;
+Canvas.onmousemove = function (e) {
+	LightPosition.x = e.offsetX == undefined ? e.layerX : e.offsetX;
+	LightPosition.y = e.offsetY == undefined ? e.layerY : e.offsetY;
 }
 
 
-function collisionDetection(b){
+function collisionDetection(b) {
 	for (var i = boxes.length - 1; i >= 0; i--) {
-		if(i != b){	
-			var dx = (boxes[b].x + boxes[b].half_size) - (boxes[i].x + boxes[i].half_size);
-			var dy = (boxes[b].y + boxes[b].half_size) - (boxes[i].y + boxes[i].half_size);
+		if (i != b) {
+			var dx = (boxes[b].x + boxes[b].HalfSize) - (boxes[i].x + boxes[i].HalfSize);
+			var dy = (boxes[b].y + boxes[b].HalfSize) - (boxes[i].y + boxes[i].HalfSize);
 			var d = Math.sqrt(dx * dx + dy * dy);
-			if (d < boxes[b].half_size + boxes[i].half_size) {
-				boxes[b].half_size = boxes[b].half_size > 1 ? boxes[b].half_size-=1 : 1;
-				boxes[i].half_size = boxes[i].half_size > 1 ? boxes[i].half_size-=1 : 1;
+			if (d < boxes[b].HalfSize + boxes[i].HalfSize) {
+				boxes[b].HalfSize = boxes[b].HalfSize > 1 ? boxes[b].HalfSize -= 1 : 1;
+				boxes[i].HalfSize = boxes[i].HalfSize > 1 ? boxes[i].HalfSize -= 1 : 1;
 			}
 		}
 	}
